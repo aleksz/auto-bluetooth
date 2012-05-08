11ece1554ff99c137378f88881dbd052034baab0
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2012-05-08 20:22:17 +0300

    Making text preferences mandatory.

--- src/com/gmail/at/zhuikov/aleksandr/ab/SettingsActivity.java
+++ src/com/gmail/at/zhuikov/aleksandr/ab/SettingsActivity.java
@@ -2,19 +2,22 @@ package com.gmail.at.zhuikov.aleksandr.ab;
 
 import static android.text.InputType.TYPE_CLASS_NUMBER;
 import static android.text.TextUtils.isEmpty;
+import static android.widget.Toast.makeText;
 import static com.gmail.at.zhuikov.aleksandr.ab.util.Preferences.CONNECTION_RETRY_INTERVAL_PREFERENCE_KEY;
 import static com.gmail.at.zhuikov.aleksandr.ab.util.Preferences.CONNECTION_TIMEOUT_PREFERENCE_KEY;
 import static com.gmail.at.zhuikov.aleksandr.ab.util.Preferences.ON_OFF_PREFERENCE_KEY;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.text.TextUtils;
 
 import com.gmail.at.zhuikov.aleksandr.ab.service.CancelPendingIntentsService;
 import com.gmail.at.zhuikov.aleksandr.ab.service.EntryPointService;
 import com.gmail.at.zhuikov.aleksandr.ab.util.Preferences;
 import com.gmail.at.zhuikov.aleksandr.ab.util.PreferencesImpl;
-
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity implements
 		SharedPreferences.OnSharedPreferenceChangeListener {
@@ -32,13 +35,17 @@ public class SettingsActivity extends PreferenceActivity implements
 		getPreferenceManager().setSharedPreferencesName(preferences.getPreferencesFileName());
 		addPreferencesFromResource(R.xml.settings);
 
+		OnPreferenceChangeListener onPreferenceChangeListener = new MandatoryPreferenceChangeListener();
+
 		connectionTimeoutPreference = (EditTextPreference) getPreferenceScreen()
 				.findPreference(CONNECTION_TIMEOUT_PREFERENCE_KEY);
 		connectionTimeoutPreference.getEditText().setInputType(TYPE_CLASS_NUMBER);
+		connectionTimeoutPreference.setOnPreferenceChangeListener(onPreferenceChangeListener);
 
 		connectionRetryIntervalPreference = (EditTextPreference) getPreferenceScreen()
 				.findPreference(CONNECTION_RETRY_INTERVAL_PREFERENCE_KEY);
 		connectionRetryIntervalPreference.getEditText().setInputType(TYPE_CLASS_NUMBER);
+		connectionRetryIntervalPreference.setOnPreferenceChangeListener(onPreferenceChangeListener);
 	}
 
 	@Override
@@ -86,5 +93,20 @@ public class SettingsActivity extends PreferenceActivity implements
 
 	EditTextPreference getConnectionRetryIntervalPreference() {
 		return connectionRetryIntervalPreference;
+	}
+
+	private class MandatoryPreferenceChangeListener implements
+			Preference.OnPreferenceChangeListener {
+
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+
+			if (isEmpty((CharSequence) value)) {
+				makeText(SettingsActivity.this, R.string.empty_preference, 0).show();
+				return false;
+			}
+
+			return true;
+		}
 	}
 }
